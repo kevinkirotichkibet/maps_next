a2a3aa28080009603fa7ad9c66f534a72da823d4
Kevin Kirotich Kibet <kirotichkevin@gmail.com>
2023-04-17 14:38:27 +0300

    Maps loading

--- pages/index.js
+++ pages/index.js
@@ -4,10 +4,33 @@ import { Inter } from "next/font/google";
 import styles from "@/styles/Home.module.css";
 
 import { useMemo } from "react";
-import { GoogleMap, useLoadScript,Marker } from "@react-google-maps/api";
+import { GoogleMap, useLoadScript, MarkerF } from "@react-google-maps/api";
 
 export default function Home() {
-  
+  const { isLoaded } = useLoadScript({
+    googleMapsApiKey: process.env.NEXT_PUBLIC_GOOGLE_MAPS_API_KEY,
+  });
 
-  return <div>Map</div>;
+  if (!isLoaded) return <div>Loading..</div>;
+  return (
+    <div>
+      <Map />
+    </div>
+  );
+
+  function Map() {
+    const center = useMemo(()=>({ lat: 1.2879, lng: 36.825 }),[]);
+
+
+
+    return (
+      <GoogleMap
+        zoom={10}
+        center={center}
+        mapContainerClassName="map-container"
+      >
+        <MarkerF position={{ lat: -1.2879, lng: 36.825 }} />
+      </GoogleMap>
+    );
+  }
 }
