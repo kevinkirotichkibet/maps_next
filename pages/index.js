77eabc954273e8f4f5366ae74f8a994a7e5d1fc0
Kevin Kirotich Kibet <kirotichkevin@gmail.com>
2023-04-17 15:50:31 +0300

    Index.js update

--- pages/index.js
+++ pages/index.js
@@ -3,12 +3,26 @@ import Image from "next/image";
 import { Inter } from "next/font/google";
 import styles from "@/styles/Home.module.css";
 
-import { useMemo } from "react";
+import { useMemo, useState } from "react";
 import { GoogleMap, useLoadScript, MarkerF } from "@react-google-maps/api";
+import usePlacesAutocomplete, {
+  getGeocode,
+  getLatLng,
+} from "use-places-autocomplete";
+
+import {
+  Combobox,
+  ComboboxInput,
+  ComboboxPopover,
+  ComboboxList,
+  ComboboxOption,
+} from "react-widgets";
+import "react-widgets/styles.css";
 
 export default function Home() {
   const { isLoaded } = useLoadScript({
     googleMapsApiKey: process.env.NEXT_PUBLIC_GOOGLE_MAPS_API_KEY,
+    libraries: ["places"],
   });
 
   if (!isLoaded) return <div>Loading..</div>;
@@ -19,18 +33,40 @@ export default function Home() {
   );
 
   function Map() {
-    const center = useMemo(()=>({ lat: 1.2879, lng: 36.825 }),[]);
-
-
+    const center = useMemo(() => ({ lat: 43.45, lng: -80.49 }), []);
+    const [selected, setSelected] = useState(null);
 
     return (
-      <GoogleMap
-        zoom={10}
-        center={center}
-        mapContainerClassName="map-container"
-      >
-        <MarkerF position={{ lat: -1.2879, lng: 36.825 }} />
-      </GoogleMap>
+      <>
+        <div className="places-container">
+          {/* <PlacesAutocomplete /> */}
+          <PlacesAutocomplete setSelected={setSelected} />
+        </div>
+
+        <GoogleMap
+          zoom={10}
+          center={center}
+          mapContainerClassName="map-container"
+        >
+          {selected && <MarkerF position={selected} />}
+          {/* <MarkerF position={{ lat: 43.45, lng: -80.49 }} /> */}
+        </GoogleMap>
+      </>
     );
   }
 }
+const PlacesAutocomplete = ({ setSelected }) => {
+  const {
+    ready,
+    value,
+    setValue,
+    suggestions: { status, data },
+    clearSuggestions,
+  } = usePlacesAutocomplete();
+  return;
+  <div>
+    <Combobox>
+      <ComboboxInput value={value} onChange={(e)=> setValue(e.target.value)}/>
+    </Combobox>
+  </div>;
+};
